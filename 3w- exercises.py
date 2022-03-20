0d082f6189deef61f4d7eb4a6e6e88c780f334f2
aviva <avivaayasso@icloud.com>
2022-03-20 20:37:10 +0200

    w3-Exercises

--- 3w- exercises.py
+++ 3w- exercises.py
@@ -6,3 +6,18 @@ if temp=="cel":
 elif temp=="fah":
     f = int(input("enter temp in fah"))
     print("the temp is", ((f-32)*5)/90, "Celsius")
+
+#3
+x=sorted(list,reverse=True)
+print(x)
+
+#4
+print(len(list))
+
+#5
+new.insert(1,78)
+print(new)
+
+#6
+del new[3]
+print(new)
